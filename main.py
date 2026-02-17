f2f57bd9c9036414191a00799ebdd609cdb5c509
nestorgpe12ng-debug <nestor.gpe12.ng@gmail.com>
2026-02-17 10:14:03 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import os
 import telebot
 from openai import OpenAI
 
-TOKEN = os.environ.get("8295031670:AAHJuV-Op_EXkDHCwhE_FMtZWLNcLlnMrEs")
-OPENAI_KEY = os.environ.get("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")
+TOKEN = os.environ.get("TOKEN")
+OPENAI_KEY = os.environ.get("OPENAI_KEY")
 
 bot = telebot.TeleBot(TOKEN)
 client = OpenAI(api_key=OPENAI_KEY)
